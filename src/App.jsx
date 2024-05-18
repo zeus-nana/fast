b7c51eae86e53c15143dd155f18cd2dfed9c370c
Zeus <fnana@ufipay.local>
2024-05-18 18:46:47 +0100

    Commit initial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+createBrowserRouter();
+
+function App() {
+  return <div>Hello!</div>;
+}
+
+export default App;
